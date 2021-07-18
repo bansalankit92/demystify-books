44fe335f8d34f4654334048bf66510528f9cd5cb
Ankit Bansal <ankit.bansal@gojek.com>
2021-07-18 14:08:42 +0530

    solved window error on build

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
-import Valine from 'valine';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
